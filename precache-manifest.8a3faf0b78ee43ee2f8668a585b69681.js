5c302d740d32d1161f09e32d71a5fbc588e8c35c
jizhenwei <jizhenwei@bytedance.com>
2020-09-09 14:28:34 +0800

    done

---
+++ precache-manifest.8a3faf0b78ee43ee2f8668a585b69681.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1725a572906444b4f7982ad4e27155f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "811ad1593ba12a26090c",
+    "url": "/static/css/main.efda7ab3.chunk.css"
+  },
+  {
+    "revision": "4b6f98a74c1c02410b15",
+    "url": "/static/js/2.06c67424.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.06c67424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811ad1593ba12a26090c",
+    "url": "/static/js/main.c48c5dbc.chunk.js"
+  },
+  {
+    "revision": "bca2f208509ed9d2e542",
+    "url": "/static/js/runtime-main.dbd73d5b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
